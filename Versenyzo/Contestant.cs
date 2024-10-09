21d201e405b57d9fa2823e3b6102897c6667d31a
Kevin <arpaskevin11@gmail.com>
2024-10-09 12:16:18 +0200

    Add project files.

---
+++ Versenyzo/Contestant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Versenyzo
+{
+    internal class Contestant
+    {
+        public Contestant(string name, List<int> scores)
+        {
+            Name = name;
+            Scores = scores;
+        }
+
+        public string Name { get; set; }
+        public List<int> Scores { get; set; }
+    }
+}
---
+++ Versenyzo/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualBasic;
+using System.IO;
+
+namespace Versenyzo
+{
+    internal class Program
+    {
+        public List<Contestant> contestants;
+        static void Main(string[] args)
+        {
+            List<Contestant> contestants = new List<Contestant>();
+
+            using StreamReader srSelejtezo = new(@"..\..\..\src\selejtezo.txt");
+
+            while (!srSelejtezo.EndOfStream)
+            {
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
